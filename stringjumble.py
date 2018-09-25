e367fba63424d6147b06c5cccef02dd87699ad9a
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-09-25 12:43:46 -0400

    Updated from Brython Server: 9/25/2018 12:43:44 PM

--- stringjumble.py
+++ stringjumble.py
@@ -2,24 +2,18 @@
 stringjumble.py
 Author: Jack Meehan
 Credit: https://stackoverflow.com/questions/931092/reverse-a-string-in-python
-
-
+https://stackoverflow.com/questions/4481724/convert-a-list-of-characters-into-a-string
 Assignment:
-
 The purpose of this challenge is to gain proficiency with 
 manipulating lists.
-
 Write and submit a Python program that accepts a string from 
 the user and prints it back in three different ways:
-
 * With all letters in reverse.
 * With words in reverse order, but letters within each word in 
   the correct order.
 * With all words in correct order, but letters reversed within 
   the words.
-
 Output of your program should look like this:
-
 Please enter a string of text (the bigger the better): There are a few techniques or tricks that you may find handy
 You entered "There are a few techniques or tricks that you may find handy". Now jumble it:
 ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
