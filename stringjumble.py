3c549c05a53ab5ac20e89b2f88a9e2c751f1bf81
hackmeehan <42844639+hackmeehan@users.noreply.github.com>
2018-09-26 11:59:33 -0400

    Updated from Brython Server: 9/26/2018 11:59:33 AM

--- stringjumble.py
+++ stringjumble.py
@@ -20,7 +20,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
-a = (str(input('Please enter a string of text (the bigger the better):')))
+a = (str(input('Please enter a string of text (the bigger the better): ')))
 print('You entered "' + a + '". Now jumble it:')
 a = a + " "
 
